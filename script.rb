6a107b5beb8ac0170b3798cd3cd90af3cc32d61a
Kelechi125 <kelechi125@yahoo.com>
2024-07-22 14:32:41 -0500

    Created #substrings method that receives a user input string and assigns the dictionary parameter to the dictionary array

--- script.rb
+++ script.rb
@@ -3,6 +3,11 @@
 # Based on a number received by user input
 # During each loop iteration, get user input asking for a word
 # Push the user input into the array each time
+# Create a function that takes a string as a parameter and also utilizing the dictionary array as a parameter
+# Function must search through the input string
+# And also check which elements in the dictionary array are in the string
+# When it identifies the dictionary elements that are in the input string
+# It needs to count how many times those elements appear in the input string
 
 dictionary = []
 
@@ -17,3 +22,22 @@ num.times do
 end
 
 p dictionary
+
+def substrings do |string, dictionary|
+  puts "Type something: "
+  string = gets.chomp
+
+  dictionary = dictionary
+
+  result_hash = {}
+
+  dictionary.map do |substring|
+    result = string.include?(substring)
+
+    if result == true # If true, create an initial entry inside hash, and if multiple of same word in string, increment the entry
+      result_hash[substring] = 0
+
+    end
+  end
+
+end
