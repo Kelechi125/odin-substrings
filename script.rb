11f77ee2e05d72a2bade98b920641cc138e855d9
Kelechi125 <kelechi125@yahoo.com>
2024-07-27 10:40:49 -0500

    Mapped through the array and checked if each substring existed in the words string, and if it did, create an entry in the result hash, and if an entry already existed, increment the value

--- script.rb
+++ script.rb
@@ -6,41 +6,36 @@
 # And store the results in a hash
 # The keys in the hash should be case-insensitive
 
-=begin
-def substrings
-  puts "Gimme a word: "
-  word = gets.chomp
-
-  dictionary = []
-
-  puts "Input a number: "
-  num = gets.chomp.to_i
-
-  num.times do
-    puts "Input a word: "
-    word = gets.chomp
-  
-    dictionary.push(word)
-  end
-
-  p dictionary
-end
-=end
-
 def substrings(words, array)
   result_hash = {}
 
-  substring_counts = array.reduce(result_hash) do |obj, substring|
+  #split_words = words.split(" ")
+
+  updated_hash = array.map do |substring, hash|
+    if words[substring] == substring
+      if !result_hash[substring]
+        result_hash[substring] = 0
+      end
+      result_hash[substring] += 1
+      result_hash
+    end
+    
+  end
+
+  p updated_hash
+=begin
+  substring_counts = array.reduce(result_hash = {}) do |hash, substring|
     if words.include?(substring) == true
-      if !obj[substring]
-        obj[substring] = 0
+      if !hash[substring]
+        hash[substring] = 0
       end
 
-      obj[substring]++
-      obj
+      hash[substring] += 1
+      hash
     end
   end
-  p substring_counts
+=end
+
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
